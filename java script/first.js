235b47d0ca653eee91fbfaaf2668607b0acba00c
janvi_2330 <janvikhandelwal59@gmail.com>
2025-02-10 12:13:58 +0530

    first commit

---
+++ java script/first.js
@@ -0,0 +1,18 @@
+// let age = 70;
+
+// if(age <= 18) {
+//     console.log("junior");
+// }
+// else if (age > 60) {
+//     console.log("senior");
+// }
+// else{
+//     console.log("middle")
+// }
+
+
+let age = 16;
+
+let result = age>= 18 ?  "adult" : "not adult";
+console.log(result);
+
